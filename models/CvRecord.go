2b21ee2549cca884dfdc9d3057c642eb4d5f3557
Duta-Sebastian <sebiduta2004@gmail.com>
2025-03-29 02:49:08 +0200

    Unmarshalled json for CvRecord

--- models/CvRecord.go
+++ models/CvRecord.go
@@ -1,8 +1,33 @@
 package models
 
-import "time"
+import (
+	"encoding/base64"
+	"encoding/json"
+	"time"
+)
 
 type CvRecord struct {
 	CvData      []byte    `json:"cv_data"`
 	LastChanged time.Time `json:"last_changed"`
+}
+
+func (c *CvRecord) UnmarshalJSON(data []byte) error {
+	type TempCvRecord struct {
+		CvData      string    `json:"cv_data"`
+		LastChanged time.Time `json:"last_changed"`
+	}
+
+	var temp TempCvRecord
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+
+	decodedData, err := base64.StdEncoding.DecodeString(temp.CvData)
+	if err != nil {
+		return err
+	}
+
+	c.CvData = decodedData
+	c.LastChanged = temp.LastChanged
+	return nil
 }
